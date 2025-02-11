b5c5c2fabc04f9bea95d9d54290a5f9c29723f69
aditidalvi <aditidalvi22@gmail.com>
2025-02-11 21:05:40 +0530

    Initial Commit

---
+++ MatchMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchMate
+//
+//  Created by Abcom on 11/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
